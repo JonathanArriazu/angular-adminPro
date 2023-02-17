0367b8d8fc1c0357c058c0d67174ce0fe0c76311
Jonathan Arriazu <joni.arriazu2@gmail.com>
2023-02-17 12:16:53 -0300

    logout was added to the project

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators'
+import { Router } from '@angular/router';
 
 import { environment } from '../../environments/environment';
 
 import { RegisterForm } from '../interfaces/register-form.interface';
 import { LoginForm } from '../interfaces/login-form-interface';
+
+declare const google: any;
 
 const base_url = environment.base_url;
 
@@ -14,7 +17,8 @@ const base_url = environment.base_url;
 })
 export class UsuarioService {
 
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient,
+               private router: Router ) { }
 
   crearUsuario ( formData: any ) {
     
@@ -46,6 +50,15 @@ export class UsuarioService {
           localStorage.setItem('token', resp.token)
         } )
       )
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('/login')
+
+    google.accounts.id.revoke( 'joni.arriazu2@gmail.com', () => {
+      this.router.navigateByUrl('/login');
+    })
   }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor( private usuarioService: UsuarioService ) { }
 
-  ngOnInit(): void {
+  logout() {
+    this.usuarioService.logout();
   }
 
 }
